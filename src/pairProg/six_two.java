99e2438193c77668ba3c26c70a30fe07ba8d893d
sainsh <kfsainsh@gmail.com>
2018-10-04 13:42:22 +0200

    pair prog, 6.2 mid

--- src/pairProg/six_two.java
+++ src/pairProg/six_two.java
@@ -1,9 +1,24 @@
 package pairProg;
 
+
+import java.util.Scanner;
+
 public class six_two {
 
     public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter a multidigit number: ");
+        System.out.println("mult of digits is: " + mulDigites(in.nextInt()));
+
+    }
+
+    public static int mulDigites(long digites){
+
+        int mult = 0;
+
+        return mult;
     }
 
 }
