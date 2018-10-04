98afe0e0f9741ef62dc71cec1befb30bbbf0da88
sainsh <kfsainsh@gmail.com>
2018-10-04 13:58:31 +0200

    pair prog, 6.8 mid

--- src/pairProg/six_8.java
+++ src/pairProg/six_8.java
@@ -1,9 +1,40 @@
 package pairProg;
 
+import java.util.Scanner;
+
 public class six_8 {
 
     public static void main(String[] args) {
+        Scanner in  = new Scanner(System.in);
+
+        System.out.print("Enter mile or kilometer and value");
+        String choice =  in.next();
+        double value = in.nextDouble();
+
+        if(choice == "mile"){
+            System.out.println(value + " miles is "mileToKilometer(value) + " kilometers");
+        }else if (choice == "kilometer"){
+            System.out.println(value + " kilometees is "kilometerToMile(value) + " milss");
+
+        }else System.out.println("wrong input");
+
+
 
     }
 
+    public static double mileToKilometer(double mile){
+        double kilometer=0;
+
+        kilometer = mile/1.6;
+
+    return kilometer;
+    }
+
+    public static double kilometerToMile(double kilometer){
+        double mile=0;
+
+        mile = 1.6*kilometer;
+
+        return mile;
+    }
 }
