b15022a0dfe7f0db897d369d9f139cefc09fc32e
sainsh <kfsainsh@gmail.com>
2018-10-04 13:48:40 +0200

    pair prog, 6.2 done

--- src/pairProg/six_two.java
+++ src/pairProg/six_two.java
@@ -7,22 +7,22 @@ public class six_two {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // input from user
 
-        System.out.print("enter a multidigit number: ");
-        System.out.println("mult of digits is: " + mulDigites(in.nextInt()));
+        System.out.print("enter a multidigit number: "); //prompt user to enter number
+        System.out.println("mult of digits is: " + mulDigites(in.nextInt())); // run muldigits method
 
     }
 
     public static int mulDigites(long digites){
 
-        int mult = 1;
+        int mult = 1;       //start value of return value
 
 
-        while(digites != 0) {
+        while(digites != 0) {      //stops when there are no digits left
 
-            mult *= digites % 10;
-            digites /= 10;
+            mult *= digites % 10;       //ekstraks last digit
+            digites /= 10;              //removes last digit
 
 
         }
