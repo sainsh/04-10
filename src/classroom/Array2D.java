b5baf9a63f4c1878b0609ef48d3c869b6591e7f8
sainsh <kfsainsh@gmail.com>
2018-10-04 14:53:38 +0200

    class room array2D start

---
+++ src/classroom/Array2D.java
@@ -0,0 +1,16 @@
+package classroom;
+
+public class Array2D {
+    public static void main(String[] args) {
+
+        int matrix[][] = new int[5][5];
+
+
+        for(int i =0; i <array2D.length; i++){
+            for(int j=0; i<array2D.length; j++){
+
+            }
+        }
+
+    }
+}
