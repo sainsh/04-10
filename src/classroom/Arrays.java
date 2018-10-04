8273605f84d623fd1155d0a1acf20470ef45be61
sainsh <kfsainsh@gmail.com>
2018-10-04 12:57:57 +0200

    array eksempel + for each

--- src/classroom/Arrays.java
+++ src/classroom/Arrays.java
@@ -10,16 +10,28 @@ public class Arrays {
         //marks[0] = 85
         //marks[1] = 75.5;
 
-        int marks[] = {72,85,93,29,85}; // laver et array med 5 værdier
-        int sum=0;  // sum af marks
+        int marks[] = {72, 85, 93, 29, 85}; // laver et array med 5 værdier
+        int sum = 0;  // sum af marks
 
-        for(int counter = 0; counter < marks.length; counter++){ //kører array igennem
+        for (int counter = 0; counter < marks.length; counter++) { //kører array igennem
 
             System.out.println(marks[counter]); // printer hvert mark
             sum += marks[counter]; // lægger mark til sum
         }
 
         System.out.println(sum); // printer sum ud
+
+
+        // for each forsøg
+        sum = 0;
+
+        for(int mark : marks){
+            System.out.println(mark); // printer hvert mark
+            sum += mark; // lægger mark til sum
+        }
+        System.out.println(sum); // printer sum ud
+
     }
+
 
 }
