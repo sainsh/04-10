8ba7bfca96fbec30887bddc8c715a33738e836f7
sainsh <kfsainsh@gmail.com>
2018-10-04 14:21:33 +0200

    pair prog, 6.20 mid

---
+++ src/pairProg/six_20.java
@@ -0,0 +1,30 @@
+package pairProg;
+
+import java.util.Scanner;
+
+public class six_20 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ener a string");
+        String s = in.next();
+
+        System.out.printf("number of letters in %d is %s",s,countLetter(s));
+
+    }
+
+    public static int countLetter(String s) {
+
+        int letters = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            letters++;
+
+        }
+
+
+        return letters;
+    }
+}
