63adab619f5be5db56653ba64f7fa8a1b837ee32
sainsh <kfsainsh@gmail.com>
2018-10-04 14:10:47 +0200

    pair prog, 6.12 mid

--- src/pairProg/six_12.java
+++ src/pairProg/six_12.java
@@ -4,4 +4,18 @@ public class six_12 {
     public static void main(String[] args) {
 
     }
+
+    public static void printNumber(int num1, int num2, int numberPerLine){
+
+        for(int i= num1; i < num2; i++ ){
+
+            System.out.print(i);
+            if(i%numberPerLine == 0){
+                System.out.print("\n");
+            }
+
+        }
+
+
+    }
 }
